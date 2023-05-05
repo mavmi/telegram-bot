dff11994f26386881a10d6a6ca15a8f9d4f0ed85
username <matveev.ivmi@yandex.ru>
2023-05-05 23:47:10 +0300

    [WATER-STUFF-BOT] init

--- SHAKAL-BOT/src/main/java/mavmi/telegram_bot/constants/Levels.java
+++
@@ -1,7 +0,0 @@
-package mavmi.telegram_bot.constants;
-
-public class Levels {
-    public static final int MAIN_LEVEL = 0;
-    public static final int APOLOCHEESE_LEVEL = 1;
-    public static final int DICE_LEVEL = 2;
-}
