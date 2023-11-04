cba9d59aab47933a6ac6a0c96ae8f37f2cbed98e
unknown <matveev.ivmi@yandex.ru>
2023-11-04 16:53:21 +0300

    Pause/continue notifications in water bot

--- WATER-STUFF-BOT/src/main/java/mavmi/telegram_bot/water_stuff_bot/data/water/WaterInfo.java
+++ WATER-STUFF-BOT/src/main/java/mavmi/telegram_bot/water_stuff_bot/data/water/WaterInfo.java
@@ -1,6 +1,10 @@
-package mavmi.telegram_bot.water_stuff_bot.data;
+package mavmi.telegram_bot.water_stuff_bot.data.water;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import mavmi.telegram_bot.water_stuff_bot.data.DataException;
+import org.springframework.lang.Nullable;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -8,11 +12,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @NoArgsConstructor
-@AllArgsConstructor
 public class WaterInfo {
     public static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
     public static final String NULL_STR = "null";
+    public static final String FILE_LINE_SEPARATOR = ";";
 
+    @Setter
+    @Getter
+    private Long userId;
     @Setter
     @Getter
     private String name;
@@ -21,6 +28,40 @@ public class WaterInfo {
     private Integer diff;
     private Date water;
     private Date fertilize;
+
+    public static WaterInfo fromFileString(String line) {
+        String[] splitted = line.split(FILE_LINE_SEPARATOR);
+
+        if (splitted.length != 5) {
+            throw new DataException("Invalid data line format");
+        }
+
+        try {
+            WaterInfo waterInfo = new WaterInfo();
+
+            waterInfo.setUserId(Long.parseLong(splitted[0]));
+            waterInfo.setName(splitted[1]);
+            waterInfo.setDiff(Integer.parseInt(splitted[2]));
+            waterInfo.setWater(splitted[3]);
+            waterInfo.setFertilize(splitted[4]);
+
+            return waterInfo;
+        } catch (NumberFormatException e) {
+            throw new DataException(e);
+        }
+    }
+
+    public String toFileString() {
+        return getUserId() +
+                FILE_LINE_SEPARATOR +
+                getName() +
+                FILE_LINE_SEPARATOR +
+                getDiff() +
+                FILE_LINE_SEPARATOR +
+                getWaterAsString() +
+                FILE_LINE_SEPARATOR +
+                getFertilizeAsString();
+    }
 
     public void setWater(Date water) {
         this.water = water;
@@ -38,10 +79,12 @@ public class WaterInfo {
         }
     }
 
+    @Nullable
     public Date getWaterAsDate() {
         return water;
     }
 
+    @Nullable
     public String getWaterAsString() {
         return (water == null) ? NULL_STR : DATE_FORMAT.format(water);
     }
@@ -63,10 +106,12 @@ public class WaterInfo {
 
     }
 
+    @Nullable
     public Date getFertilizeAsDate() {
         return fertilize;
     }
 
+    @Nullable
     public String getFertilizeAsString() {
         return (fertilize == null) ? NULL_STR : DATE_FORMAT.format(fertilize);
     }
--- WATER-STUFF-BOT/src/main/java/mavmi/telegram_bot/water_stuff_bot/telegram_bot/NotificationThread.java
+++ WATER-STUFF-BOT/src/main/java/mavmi/telegram_bot/water_stuff_bot/telegram_bot/NotificationThread.java
@@ -2,38 +2,34 @@ package mavmi.telegram_bot.water_stuff_bot.telegram_bot;
 
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import mavmi.telegram_bot.common.database.model.RuleModel;
-import mavmi.telegram_bot.common.database.repository.RuleRepository;
-import mavmi.telegram_bot.water_stuff_bot.data.WaterContainer;
-import mavmi.telegram_bot.water_stuff_bot.data.WaterInfo;
+import mavmi.telegram_bot.water_stuff_bot.data.pause.UsersPauseNotificationsData;
+import mavmi.telegram_bot.water_stuff_bot.data.water.UsersWaterData;
+import mavmi.telegram_bot.water_stuff_bot.data.water.WaterInfo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
 public class NotificationThread extends Thread {
     private final long sleepTime;
-    private final WaterContainer waterContainer;
-    private final RuleRepository ruleRepository;
-    private final List<Long> idx;
+    private final UsersWaterData usersWaterData;
+    private final UsersPauseNotificationsData usersPauseNotificationsData;
 
     @Setter
     private Bot telegramBot;
 
     public NotificationThread(
-            WaterContainer waterContainer,
-            RuleRepository ruleRepository,
+            UsersWaterData usersWaterData,
+            UsersPauseNotificationsData usersPauseNotificationsData,
             @Value("${bot.sleep-time}") Long sleepTime
     ) {
-        this.waterContainer = waterContainer;
-        this.ruleRepository = ruleRepository;
-        this.idx = getIdx();
+        this.usersWaterData = usersWaterData;
+        this.usersPauseNotificationsData = usersPauseNotificationsData;
         this.sleepTime = sleepTime;
     }
 
@@ -41,14 +37,19 @@ public class NotificationThread extends Thread {
     public void run() {
         while (true) {
             try {
-                String msg = generateMessage();
-                if (msg != null) {
-                    for (long id : idx) {
-                        telegramBot.sendMessage(id, msg);
-                        log.info("Message sent to id: {}", id);
+                for (Long id : usersWaterData.getUsersIdx()) {
+                    String msg = generateMessage(id);
+                    if (msg != null) {
+                        Long userPauseValue = usersPauseNotificationsData.get(id);
+                        if (userPauseValue != null && System.currentTimeMillis() < userPauseValue) {
+                            log.info("User paused notifications; id {}", id);
+                        } else {
+                            telegramBot.sendMessage(id, msg);
+                            log.info("Message sent to id: {}", id);
+                        }
+                    } else {
+                        log.debug("Message is null; id {}", id);
                     }
-                } else {
-                    log.debug("Message is null");
                 }
                 sleep(sleepTime);
             } catch (InterruptedException e) {
@@ -57,11 +58,15 @@ public class NotificationThread extends Thread {
         }
     }
 
-    private String generateMessage() {
-        StringBuilder builder = new StringBuilder();
+    @Nullable
+    private String generateMessage(Long userId) {
+        List<WaterInfo> waterInfoList = usersWaterData.getAll(userId);
+        if (waterInfoList == null || waterInfoList.isEmpty()) {
+            return null;
+        }
 
-        for (Map.Entry<String, WaterInfo> entry : waterContainer.entrySet()) {
-            WaterInfo waterInfo = entry.getValue();
+        StringBuilder builder = new StringBuilder();
+        for (WaterInfo waterInfo : waterInfoList) {
             Date waterDate = waterInfo.getWaterAsDate();
             if (waterDate == null) {
                 continue;
@@ -85,19 +90,8 @@ public class NotificationThread extends Thread {
 
         if (!builder.isEmpty()) {
             return builder.insert(0, "Нужно полить:\n").toString();
+        } else {
+            return null;
         }
-        return null;
-    }
-
-    private List<Long> getIdx() {
-        List<Long> idx = new ArrayList<>();
-
-        for (RuleModel ruleModel : ruleRepository.getAll()) {
-            if (ruleModel.getWaterStuff() != null && ruleModel.getWaterStuff()) {
-                idx.add(ruleModel.getUserid());
-            }
-        }
-
-        return idx;
     }
 }
