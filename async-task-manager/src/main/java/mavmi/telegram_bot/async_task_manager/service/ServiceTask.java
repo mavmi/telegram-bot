bab462eca401eae7588f46e4758088f9cb9f1d70
unknown <matveev.ivmi@yandex.ru>
2023-12-12 18:00:43 +0300

    mover bots from inner network

--- async-task-manager/src/main/java/mavmi/telegram_bot/async_task_manager/service/ServiceTask.java
+++ async-task-manager/src/main/java/mavmi/telegram_bot/async_task_manager/service/ServiceTask.java
@@ -1,10 +1,14 @@
 package mavmi.telegram_bot.async_task_manager.service;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ServiceTask {
     private String target;
     private String message;
